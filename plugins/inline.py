b0f2585e666df9e7701b273a0cf98ea42f8f7646
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-11 20:21:23 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ', url='https://t.me/cinemala_com1')
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
