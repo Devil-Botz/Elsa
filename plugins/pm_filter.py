26bbec2da04d9d7aa480c0e504113738cea0dbf7
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-16 12:53:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -531,7 +531,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.SONG_TXT
+            text=script.SONG_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
