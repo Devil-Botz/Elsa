3de6c74ffd93189bb5f41aaf06a3192ad5984605
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-08-16 21:02:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -376,7 +376,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 file_send=await client.send_cached_media(
                     chat_id=FILE_CHANNEL,
                     file_id=file_id,
-                    caption=script.CHANNEL_CAP,
+                    caption=script.CHANNEL_CAP.format(query.from_user.mention, title, query.message.chat.title),
                     protect_content=True if ident == "filep" else False,
                     reply_markup=InlineKeyboardMarkup(
                         [
