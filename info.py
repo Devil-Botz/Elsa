e3afba6e12a2bb99f4c04eea72eed03d861dafca
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-16 14:40:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ class evamaria(Client):
             api_id=APP_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            parse_mode="enums.ParseMode.HTML,
+            parse_mode=enums.ParseMode.HTML,
             sleep_threshold=60
         )
 
