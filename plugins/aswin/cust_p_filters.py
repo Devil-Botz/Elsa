a4d716714260ec363c5b9877d14e9618d65ed127
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-16 11:26:39 +0530

    Update cust_p_filters.py

--- plugins/aswin/cust_p_filters.py
+++ plugins/aswin/cust_p_filters.py
@@ -2,7 +2,7 @@ from pyrogram import (
     filters, enum
 )
 from info import ADMINS, AUTH_USERS
-from plugins.helper_functions.admin_check import admin_check
+from plugins.aswin.admin_check import admin_check
 import os
 
 USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
