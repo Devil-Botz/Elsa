9e86c4f45c7e20478f745aa2a610da9496782c25
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-16 15:08:00 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -49,7 +49,7 @@ def song(client, message):
         open(thumb_name, 'wb').write(thumb.content)
 
 
-        performer = f"[Elsa]" 
+        performer = f"[Elsa - Cinemala.com™]" 
         duration = results[0]["duration"]
         url_suffix = results[0]["url_suffix"]
         views = results[0]["views"]
