2ab4b78e6f6934e2fb380c440931f2018eed9a92
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-16 11:39:12 +0530

    Update telegraph.py

--- plugins/helpers/telegraph.py
+++ plugins/helpers/telegraph.py
@@ -1,10 +1,10 @@
 import os
 import shutil
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from telegraph import upload_file
 from info import TMP_DOWNLOAD_DIRECTORY
-from plugins.aswin.cust_p_filters import f_onw_fliter
-from plugins.aswin.get_file_id import get_file_id
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.helper_functions.get_file_id import get_file_id
 
 
 @Client.on_message(
