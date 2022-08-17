e20b5716c00c219c7b44becc81979ffe929ffcef
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-08-17 19:34:45 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -25,7 +25,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@Client.on_message(filters.command('song') & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command('song'))
 def song(client, message):
 
     user_id = message.from_user.id 
