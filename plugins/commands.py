f2b31ee631c62e8ef68128037fc39ec25958c35a
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-18 09:43:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,6 +45,10 @@ async def start(client, message):
             InlineKeyboardButton('Surprise', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_chat_action("Typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
+        await asyncio.sleep(1)
+        await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
