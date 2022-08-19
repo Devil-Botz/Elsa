081ac2ce70c4689d0e3d8047a89a5e9ca1d2c94e
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-08-20 01:18:52 +0530

    Update country.py

--- plugins/helper/country.py
+++ plugins/helper/country.py
@@ -4,42 +4,40 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @Client.on_message(filters.command(["country"]))
-async def country_info(update: Message):
-    country = update.text.split(" ", 1)[1]
-    country = CountryInfo(country)
-    info = f"""--**Country Information**--
+async def countryinfo(bot, update):
+    
+    country = CountryInfo(update.text)
+    
+    info = f"""**Country Information**
 
-Name : ```{country.name()}```
-Native Name : ```{country.native_name()}```
-Capital : ```{country.capital()}```
-Population : ```{country.population()}```
-Region : ```{country.region()}```
-Sub Region : ```{country.subregion()}```
-Top Level Domains : ```{country.tld()}```
-Calling Codes : ```{country.calling_codes()}``|
-Currencies : ```{country.currencies()}```
-Residence : ```{country.demonym()}```
-Timezone : ```{country.timezones()}```
+Name : `{country.name()}`
+Native Name : `{country.native_name()}`
+Capital : `{country.capital()}`
+Population : `{country.population()}`
+Region : `{country.region()}`
+Sub Region : `{country.subregion()}`
+Top Level Domains : `{country.tld()}`
+Calling Codes : `{country.calling_codes()}`
+Currencies : `{country.currencies()}`
+Residence : `{country.demonym()}`
+Timezone : `{country.timezones()}`
 
-©CINEMALA.COM """
-    country_name = country.name()
-    country_name = country_name.replace(" ", "+")
+©CINEMALA.COM"""
+    
     reply_markup=InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Wikipedia', url=f'{country.wiki()}'),
-        InlineKeyboardButton('Google', url=f'https://www.google.com/search?q={country_name}')
-        ]]
+        [
+            [
+                InlineKeyboardButton('Wikipedia', url=country.wiki()),
+                InlineKeyboardButton('Google', url=country.google())
+            ]
+        ]
     )
+    
     try:
         await update.reply_text(
             text=info,
             reply_markup=reply_markup,
-            disable_web_page_preview=True,
-            quote=True
+            disable_web_page_preview=True
         )
     except Exception as error:
-        await update.reply_text(
-            text=error,
-            disable_web_page_preview=True,
-            quote=True
-        )
+        print(error)
