8c76534a28d48cc298a82c419f9bcc090ae86bc9
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-11-28 09:51:31 +0530

    Update ai.py

--- plugins/helpers/ai.py
+++ plugins/helpers/ai.py
@@ -1,1 +1,153 @@
+from pyrogram import Client, filters
+from pyrogram.types import *
+from pymongo import MongoClient
+import requests
+import os
+import re
 
+
+
+
+async def is_admins(chat_id: int):
+    return [
+        member.user.id
+        async for member in bot.iter_chat_members(
+            chat_id, filter="administrators"
+        )
+    ]
+
+
+@bot.on_message(
+    filters.command("setupchat", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def addchat(_, message): 
+    kukidb = MongoClient(DATABASE_URI)
+    
+    kuki = kukidb["KukiDb"]["Kuki"] 
+    if message.from_user:
+        user = message.from_user.id
+        chat_id = message.chat.id
+        if user not in (
+            await is_admins(chat_id)
+        ):
+            return await message.reply_text(
+                "You are not admin"
+            )
+    is_kuki = kuki.find_one({"chat_id": message.chat.id})
+    if not is_kuki:
+        kuki.insert_one({"chat_id": message.chat.id})
+        await message.reply_text(f"✅ | Successfully\nKuki Chatbot of this Group is set to @{message.chat.username}\n Requested by [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n© @cinemala_com1")
+    else:
+        await message.reply_text(f"Already Setup Kuki Chatbot of this Group Is @{message.chat.username}")
+
+
+@bot.on_message(
+    filters.command("removechat", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def rmchat(_, message): 
+    kukidb = MongoClient(DATABASE_URI)
+    
+    kuki = kukidb["KukiDb"]["Kuki"] 
+    if message.from_user:
+        user = message.from_user.id
+        chat_id = message.chat.id
+        if user not in (
+            await is_admins(chat_id)
+        ):
+            return await message.reply_text(
+                "You are not admin"
+            )
+    is_kuki = kuki.find_one({"chat_id": message.chat.id})
+    if not is_kuki:
+        await message.reply_text("Already Kuki ChatBot Disable")
+    else:
+        kuki.delete_one({"chat_id": message.chat.id})
+        await message.reply_text("✅ | Kuki Chatbot is disable!")
+
+
+
+
+
+@bot.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.private
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
+async def kukiai(client: Client, message: Message):
+
+   kukidb = MongoClient(DATABASE_URI)
+    
+   kuki = kukidb["KukiDb"]["Kuki"] 
+
+   is_kuki = kuki.find_one({"chat_id": message.chat.id})
+   if is_kuki:
+       if message.reply_to_message:      
+           botget = await bot.get_me()
+           botid = botget.id
+           if not message.reply_to_message.from_user.id == botid:
+               return
+           await bot.send_chat_action(message.chat.id, "typing")
+           if not message.text:
+               msg = "/"
+           else:
+               msg = message.text
+           try: 
+               x = requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
+               x = x['reply']
+               await asyncio.sleep(1)
+           except Exception as e:
+               error = str(e)
+           await message.reply_text(x)
+           await bot.send_message(
+           ERROR_LOG, f"""{error}""")
+           await bot.send_chat_action(message.chat.id, "cencel") 
+   
+
+
+@bot.on_message(
+    filters.text
+    & filters.reply
+    & filters.private
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
+async def kukiai(client: Client, message: Message):
+    await bot.send_chat_action(message.chat.id, "typing")
+    if not message.text:
+        msg = "/"
+    else:
+        msg = message.text
+    try:
+        x = requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
+        x = x['reply']
+        await asyncio.sleep(1)
+    except Exception as e:
+        ERROR = str(e)
+    await message.reply_text(x)
+    await bot.send_message(
+           ERROR_LOG, f"""{ERROR}""")
+    await bot.send_chat_action(message.chat.id, "cancel")
+
+
+
+@bot.on_message(
+    filters.command("chat", prefixes=["/", ".", "?", "-"]))
+async def kukiai(client: Client, message: Message):
+    await bot.send_chat_action(message.chat.id, "typing")
+    if not message.text:
+        msg = "/"
+    else:
+        msg = message.text.replace(message.text.split(" ")[0], "")
+    try:
+        x = requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
+        x = x['reply']
+        await asyncio.sleep(1)
+    except Exception as e:
+        ERROR = str(e)
+    await bot.send_message(
+           ERROR_LOG, f"""{ERROR}""")
+    await message.reply_text(x)
