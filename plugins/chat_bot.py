cf594b692c239b144708d3f75d5bf04f478ca23d
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-04 19:29:44 +0530

    Update chat_bot.py

--- plugins/chat_bot.py
+++ plugins/chat_bot.py
@@ -10,7 +10,7 @@ from info import MONGO_URL, ERROR_LOG, KUKI_API
 async def is_admins(chat_id: int):
     return [
         member.user.id
-        async for member in bot.iter_chat_members(
+        async for member in Client.iter_chat_members(
             chat_id, filter="administrators"
         )
     ]
