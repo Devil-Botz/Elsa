85232759a291082f6bd725a32b10d205c864f5ac
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-13 20:14:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/2992a480cae2bc0de1c39.jpg https://telegra.ph/file/76e7b5e94430b84a3d2b2.jpg https://telegra.ph/file/3544a8773740b0412c9dd.jpg https://telegra.ph/file/4b1c7004ea8bd3fed8df9.jpg https://telegra.ph/file/a02e47d932adc336740fa.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/7d7cbf0d6c39dc5a05f5a.jpg")
-SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/c0208e04c7af4ac5000f9.jpg")
+SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/7aa8f44130b6ba9c88f23.jpg")
 
 # Welcome area
 MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/b4478b5275a94904d9198.jpg")
