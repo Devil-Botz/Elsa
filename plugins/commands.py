9d3767cd5ae293f20eda27d5a04223305e3f94e7
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-18 13:51:48 +0530

    PreDVD CamRip remove from db

--- plugins/commands.py
+++ plugins/commands.py
@@ -528,3 +528,13 @@ async def save_template(client, message):
     template = message.text.split(" ", 1)[1]
     await save_group_settings(grp_id, 'template', template)
     await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
+
+@Client.on_message(filters.command("deletefiles") & filters.user(ADMINS))
+async def deletemultiplefiles(bot, message):
+    btn = [[
+            InlineKeyboardButton("Delete PreDVDs", callback_data="predvd"),
+            InlineKeyboardButton("Delete CamRips", callback_data="camrip")
+          ]]
+    await message.reply_text(
+        text="<b>Select the type of files you want to delete !\n\nThis will delete 100 files from the database for the selected type.</b>",
+        reply_markup=InlineKeyboardMarkup(btn)
