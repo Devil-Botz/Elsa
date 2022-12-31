efed903f81e96f553819e00f8d48ec453f105a5b
illuzX <98640218+illuzX@users.noreply.github.com>
2022-12-31 14:19:29 +0530

    Worker Count increased 🧲
    requests.exceptions.HTTPError: 429 Client Error: Too Many Requests for url: https://www.google.com/sorry/index?continue=https://www.google.com/

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=150,
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
