564d0780295ab088f882ed2cc0eb54700537ba9d
soulpc <119080077+soulpc@users.noreply.github.com>
2023-07-11 09:12:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/soulpc/Elsa
+  git clone https://github.com/soulpc/Elsa.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
