404831731ca9b5df9152a99d4cf32a118842d07b
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-08-30 12:40:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,7 +240,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❤️‍🔥 ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ ❤️‍🔥', url=(MAIN_CHANNEL)) ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔰 JOIN ❗️C H A N N E L 🔰', url=(MAIN_CHANNEL)) ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
