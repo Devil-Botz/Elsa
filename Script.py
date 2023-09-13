5154816d199688bd3c162ee7cbebae26fb0baa84
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-09-13 17:34:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
     ABOUT_TXT = """<b> 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
  𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a https://t.me/ARAKAL_THERAVAD_MOVIES>ARAKAL_THERAVAD_MOVIES.𝙲𝙾𝙼</a>
  𝚃𝙷𝙰𝙽𝙺𝚂 𝚃𝙾: <a href=https://t.me/oTTGArakalTheravadMovies>ARAKAL_THERAVAD_MOVIES</a>
- 𝙳𝙴𝚅: <a https://t.me/ARAKAL_THERAVAD_MOVIES>『ARAKAL_THERAVAD_MOVIES』</a>
+ 𝙳𝙴𝚅: <a https://t.me/ARAKAL_THERAVAD_MOVIES> ARAKAL_THERAVAD_MOVIES </a>
  𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
  𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
  𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
