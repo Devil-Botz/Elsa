8ae1cb24664137f5d2c56c917d5b1d8c643cd7c8
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-10-10 10:23:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 600))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/715d2668aadfb32cf55de.jpg https://telegra.ph/file/76e7b5e94430b84a3d2b2.jpg https://telegra.ph/file/3544a8773740b0412c9dd.jpg https://telegra.ph/file/4b1c7004ea8bd3fed8df9.jpg https://telegra.ph/file/a02e47d932adc336740fa.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/715d2668aadfb32cf55de.jpg")
-SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/f044938bb933f4f7ace82.jpg")
+SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/89b856e886483f9019094.jpg")
 
 # Welcome area
 MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/e54cae941b9b81f13eb71.jpg")
