c6e685b97ee7ab6a34b0b63a0f8d2046eed19409
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-10-21 12:25:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -515,7 +515,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                  InlineKeyboardButton(f'Tᴀᴍ', 'ftam'),
                                  InlineKeyboardButton(f'Hɪɴ', 'fhin')
                               ],[
-                                 InlineKeyboardButton( 🔰 JOIN ❗️C H A N N E L 🔰 ", url=(MAIN_CHANNEL))
+                                 InlineKeyboardButton(  JOIN  C H A N N E L  ", url=(MAIN_CHANNEL))
                                 ]
                             ]
                         )
